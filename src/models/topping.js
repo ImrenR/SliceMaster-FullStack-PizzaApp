73a7d62a2dae8a76e7c5acf4a084228b17f1cd14
ImrenR <imrenrahba@gmail.com>
2025-05-03 23:05:07 +0200

    add toppings model

--- src/models/topping.js
+++ src/models/topping.js
@@ -4,3 +4,20 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+const ToppingsSchema = new mongoose.Schema(
+    {
+      name: {
+        type:String,
+        trim: true,
+        required: true,
+        unique: true,
+      },
+    },
+    {
+      collection: "toppings",
+      timestamps: true,
+    }
+  );
+  /* ------------------------------------------------------- */
+  module.exports = mongoose.model("Toppings", ToppingsSchema);
+  /* ------------------------------------------------------- */
