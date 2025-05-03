55ee18d18553249b364802c57af6e3ace06c67a8
ImrenR <imrenrahba@gmail.com>
2025-05-03 22:29:55 +0200

    add order controller

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -2,3 +2,77 @@
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
+const Order = require("../models/order");
+
+module.exports = {
+  list: async (req, res) => {
+    /* 
+          
+    #swagger.ignore = true
+        */
+
+    const result = await res.getModelList(Order);
+
+    res.status(200).send({
+      error: false,
+      details: await getModelListDetails(Order),
+      result,
+    });
+  },
+
+  create: async (req, res) => {
+     /* 
+          
+    #swagger.ignore = true
+        */
+  // To be added after Pizza
+    const result = await Order.create(req.body);
+
+    res.status(201).send({
+      error: false,
+      result,
+    });
+  },
+
+  read: async (req, res) => {
+    /* 
+          
+    #swagger.ignore = true
+        */
+  const result= await Order.findOne({_id:req.params.id}, populate(["userId","pizzaId"]));
+
+    res.status(200).send({
+      error: false,
+      result,
+    });
+  },
+
+  update: async (req, res) => {
+     /* 
+          
+    #swagger.ignore = true
+        */
+    const result= await Order.findByIdAndUpdate({ _id: req.params.id},req.body,{runValidators:true,new:true})
+// runValidators:true will run the validators on the updated data
+// new:true will return the updated data
+// if you don't use new:true, it will return the old data
+    
+    res.status(200).send({
+      error: false,
+     result,
+    });
+  },
+  delete: async (req, res) => {
+
+  /*  #swagger.tags = ['Orders']
+            #swagger.summary = 'Delete Orders'  
+    */
+    const result= await Order.deleteOne({ _id: req.params.id});
+// deleteOne will delete the document and return the result
+// if you want to delete multiple documents, use deleteMany
+    res.status(result.deletedCount ? 204 : 404).send({ 
+      error: !deletedCount,
+     result,
+    });
+  },
+};
