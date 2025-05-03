700a5872285b0458b720f349c820948bb32b1d32
ImrenR <imrenrahba@gmail.com>
2025-05-03 22:46:51 +0200

    add order and pizza

--- src/controllers/pizza.js
+++ src/controllers/pizza.js
@@ -2,3 +2,78 @@
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
+
+const Pizza = require("../models/pizza.js");
+
+module.exports = {
+  list: async (req, res) => {
+    /* 
+          
+    #swagger.ignore = true
+        */
+
+    const result = await res.getModelList(Pizza);
+
+    res.status(200).send({
+      error: false,
+      details: await getModelListDetails(Pizza),
+      result,
+    });
+  },
+
+  create: async (req, res) => {
+     /* 
+          
+    #swagger.ignore = true
+        */
+  // To be added after Pizza
+    const result = await Pizza.create(req.body);
+
+    res.status(201).send({
+      error: false,
+      result,
+    });
+  },
+
+  read: async (req, res) => {
+    /* 
+          
+    #swagger.ignore = true
+        */
+  const result= await Pizza.findOne({_id:req.params.id}, populate(["userId","pizzaId"]));
+
+    res.status(200).send({
+      error: false,
+      result,
+    });
+  },
+
+  update: async (req, res) => {
+     /* 
+          
+    #swagger.ignore = true
+        */
+    const result= await Pizza.findByIdAndUpdate({ _id: req.params.id},req.body,{runValidators:true,new:true})
+// runValidators:true will run the validators on the updated data
+// new:true will return the updated data
+// if you don't use new:true, it will return the old data
+    
+    res.status(200).send({
+      error: false,
+     result,
+    });
+  },
+  delete: async (req, res) => {
+
+  /*  #swagger.tags = ['Pizzas']
+            #swagger.summary = 'Delete Pizzas'  
+    */
+    const result= await Pizza.deleteOne({ _id: req.params.id});
+// deleteOne will delete the document and return the result
+// if you want to delete multiple documents, use deleteMany
+    res.status(result.deletedCount ? 204 : 404).send({ 
+      error: !deletedCount,
+     result,
+    });
+  },
+};
--- src/models/pizza.js
+++ src/models/pizza.js
@@ -4,3 +4,31 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const PizzaSchema=new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+      },
+      
+      image:String,
+
+      price:{
+        type:Number,
+        required:true
+      },
+
+      
+      toppingIds:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Topping"
+        }
+      ]
+
+
+},{collection:"pizzas",timestamps:true})
+
+module.exports=mongoose.model("Pizza",PizzaSchema)
