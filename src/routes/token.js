e2dd34a5dc7d6c5c4d05e8c19adbe78fb108246d
ImrenR <imrenrahba@gmail.com>
2025-05-03 22:12:01 +0200

    add token router

--- src/routes/token.js
+++ src/routes/token.js
@@ -1,9 +1,17 @@
-"use strict"
-/* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
-------------------------------------------------------- */
-const router = require('express').Router()
+"use strict";
 /* ------------------------------------------------------- */
 
+const router = require("express").Router();
+const token = require("../controllers/token");
 /* ------------------------------------------------------- */
-module.exports = router
+router.route("/").get(token.list).post(token.create);
+
+router
+  .route("/:id")
+  .get(token.read)
+  .put(token.update)
+  .patch(token.update)
+  .delete(token.delete);
+/* ------------------------------------------------------- */
+module.exports = router;
+/* ------------------------------------------------------- */
