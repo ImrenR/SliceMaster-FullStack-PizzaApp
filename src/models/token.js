5e9d9ac623b2a05f2bc46520523b0b5d0ba08c91
ImrenR <imrenrahba@gmail.com>
2025-05-03 22:03:45 +0200

    add token model

--- src/models/token.js
+++ src/models/token.js
@@ -1,6 +1,26 @@
 "use strict"
-/* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
-------------------------------------------------------- */
+/* ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+const TokenSchema = new mongoose.Schema({
+     
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', // Reference to User model
+        required: true,
+        unique: true
+    },
+    token:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    }
+
+
+},{
+    collection: 'tokens',
+    timestamps: true    
+})
+
+module.exports = mongoose.model('Token', TokenSchema)
