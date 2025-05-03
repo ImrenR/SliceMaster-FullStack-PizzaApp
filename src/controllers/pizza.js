baabdff364f3a3b67c3fb82719d6929febe6a992
ImrenR <imrenrahba@gmail.com>
2025-05-03 23:00:38 +0200

    add pizza controller

--- src/controllers/pizza.js
+++ src/controllers/pizza.js
@@ -7,12 +7,8 @@ const Pizza = require("../models/pizza.js");
 
 module.exports = {
   list: async (req, res) => {
-    /* 
-          
-    #swagger.ignore = true
-        */
-
-    const result = await res.getModelList(Pizza);
+   
+    const result = await res.getModelList(Pizza, "toppingIds");
 
     res.status(200).send({
       error: false,
@@ -26,7 +22,9 @@ module.exports = {
           
     #swagger.ignore = true
         */
-  // To be added after Pizza
+  req.body.toppingIds=[...new Set(req.body.toppingIds)]
+// this will remove duplicates from the array
+
     const result = await Pizza.create(req.body);
 
     res.status(201).send({
@@ -40,24 +38,32 @@ module.exports = {
           
     #swagger.ignore = true
         */
-  const result= await Pizza.findOne({_id:req.params.id}, populate(["userId","pizzaId"]));
+  const result= await Pizza.findOne({_id:req.params.id}, populate("toppingIds"))
+// populate will replace the toppingIds with the actual data from the Topping model;
+// this will return the pizza with the toppingIds replaced with the actual data
+// if you want to populate multiple fields, use populate({ path: "field1 field2" })
 
     res.status(200).send({
       error: false,
       result,
-    });
-  },
+        });
+    },
 
   update: async (req, res) => {
-     /* 
-          
+
+     /*    
     #swagger.ignore = true
-        */
-    const result= await Pizza.findByIdAndUpdate({ _id: req.params.id},req.body,{runValidators:true,new:true})
-// runValidators:true will run the validators on the updated data
-// new:true will return the updated data
-// if you don't use new:true, it will return the old data
-    
+    */
+
+    const result= await Pizza.findByIdAndUpdate({ _id: req.params.id},req.body,{
+        runValidators:true,
+        new:true
+    });
+if(!result.modifiedCount){
+    res.errorStatusCode=404
+      throw new Error ("Pizza not found")
+    }
+
     res.status(200).send({
       error: false,
      result,
@@ -69,8 +75,6 @@ module.exports = {
             #swagger.summary = 'Delete Pizzas'  
     */
     const result= await Pizza.deleteOne({ _id: req.params.id});
-// deleteOne will delete the document and return the result
-// if you want to delete multiple documents, use deleteMany
     res.status(result.deletedCount ? 204 : 404).send({ 
       error: !deletedCount,
      result,
