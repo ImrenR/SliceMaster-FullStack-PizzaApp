b5991486055dfbdc76bd78d22a551ee2769d6e04
ImrenR <imrenrahba@gmail.com>
2025-05-04 14:26:36 +0200

    final auth controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,10 +1,12 @@
-'use strict';
+"use strict";
+const { token } = require("morgan");
 const CustomError = require("../helpers/customError");
-const User  = require("../models/user");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+const User = require("../models/user");
 
 module.exports = {
-    login: async (req, res) => {
-        /*
+  login: async (req, res) => {
+    /*
             #swagger.tags = ["Authentication"]
             #swagger.summary = "Login"
             #swagger.description = 'Login with username (or email) and password for get simpleToken and JWT'
@@ -17,38 +19,51 @@ module.exports = {
                 }
             }
         */
-        // Implement login logic here
+    // Implement login logic here
 
-        const { username,email, password } = req.body;
-         
-        if(!(username || email) && !password) throw new CustomError("Username or email and password are required", 401);
-         
-        const user = await User.findOne({$or: [{username}, {email}], password}); // find user by username or email and password
-        if(!user) throw new CustomError("Invalid username/email or password", 401);
-        if(!user.isActive) throw new CustomError("User is not active", 401);
-        
-        res.status(200).send({ 
-            error: false,
-            message: 'Login successful' 
-        });
-    },
-    logout: async (req, res) => {
-        // Implement logout logic here
-        res.status(200).send({ 
-            error: false,
-            message: 'Logout successful' 
-        });
-    },
-    refresh: async (req, res) => {
-        // Implement refresh logic here
-        res.status(200).send({ 
-            error: false,
-            message: 'Token refreshed successfully' 
-        });
+    const { username, email, password } = req.body;
+
+    if (!(username || email) && !password)
+      throw new CustomError("Username or email and password are required", 401);
+
+    const user = await User.findOne({
+      $or: [{ username }, { email }],
+      password,
+    }); // find user by username or email and password
+    if (!user) throw new CustomError("Invalid username/email or password", 401);
+    if (!user.isActive) throw new CustomError("User is not active", 401);
+    /* Simple Token */
+    /*  #to see whether the user has token  or not */
+
+    let tokenData = await User.findOne({ userId: user._id });
+    if (!tokenData) {
+      tokenData = await Token.create({
+        userId: user._id,
+        token: passwordEncrypt(Date.now() + user._id),
+      });
     }
-    // Add more functions as needed
-    
-    
-    
 
-}
+    /* Simple Token */
+
+    res.status(200).send({
+      error: false,
+      token: tokenData.token,
+      message: "Login successful",
+    });
+  },
+  logout: async (req, res) => {
+    // Implement logout logic here
+    res.status(200).send({
+      error: false,
+      message: "Logout successful",
+    });
+  },
+  refresh: async (req, res) => {
+    // Implement refresh logic here
+    res.status(200).send({
+      error: false,
+      message: "Token refreshed successfully",
+    });
+  },
+  // Add more functions as needed
+};
